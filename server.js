90533435ddbc0e0e982d08e4d96946ed3a3353e4
Vigneswari Sambandam <vigneshwarie@gmail.com>
2023-12-12 19:56:50 -0500

    Updated server file for note redirection

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-// Import express package
+// Import express and path package
 const express = require('express');
+const path = require('path');
 
 // Initialize express
 const app = express();
 
+// Middleware to parse JSON and form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Invoking all static files through public folder
+app.use(express.static('public'));
+
 // The root URL to perform task
-app.get('/', (req, res) => { });
+app.get('/', (req, res) => res.send('Please navigate to the Notes Keeper /notes'));
+
+// URL to navigate to the notes HTML page.
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 // Set the port
 const PORT = process.env.PORT || 3001;
